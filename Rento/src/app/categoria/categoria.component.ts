879d9486a8b2180fb076981c0c5c49516a7102d6
oGraebin <felipegraebin@hotmail.com>
2021-11-18 17:18:10 -0300

    :rocket: Implementação de cards para serviços e modal

--- Rento/src/app/categoria/categoria.component.ts
+++ Rento/src/app/categoria/categoria.component.ts
@@ -5,7 +5,9 @@ import { environment } from './../../environments/environment.prod';
 import { CategoriaService } from './../service/categoria.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Produto } from '../model/Produto';
-import { ProdutoService } from '../service/produto.service'; //
+import { ProdutoService } from '../service/produto.service';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-categoria',
@@ -22,17 +24,23 @@ export class CategoriaComponent implements OnInit {
   produto: Produto = new Produto()
   listaProduto: Produto[]
 
+  idProduto: number // teste
+  Usuario: Usuario = new Usuario()
+
+
   constructor(
     private router: Router,
     private categoriaService: CategoriaService,
     private http: HttpClient,
     private produtoService: ProdutoService,
+    private authService: AuthService
+
     
   ) { }
 
   ngOnInit() {
     // retirar apos testes
-    environment.token = "Basic Z3VzdGF2b0B0ZXN0ZS5jb206MTIzNDU2Nzg5"
+    // environment.token = "Basic Z3VzdGF2b0B0ZXN0ZS5jb206MTIzNDU2Nzg5"
     // retirar apos testes
     window.scroll(0,0)
     if(environment.token == ''){
@@ -59,11 +67,13 @@ export class CategoriaComponent implements OnInit {
      })
   }
   // teste gus
+  /*
   findByIdCategoria(){
     this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria) =>{
       this.categoria = resp
     })
   }
+  */
   getAllProduto(){
     this.produtoService.getAllProduto().subscribe((resp: Produto[]) => {
       this.listaProduto = resp
@@ -71,4 +81,17 @@ export class CategoriaComponent implements OnInit {
     console.log(this.listaProduto)
   }
 
+  // getAllUsuario(){
+  //   this.authService.getAllUsuario().subscribe((resp: Usuario) => {
+  //     this.Usuario = resp
+  //   })
+  //   console.log(this.Usuario)
+  // }
+
+  // getByIdProduto(id: number){
+  //   this.produtoService.getByIdProduto(id).subscribe((resp: Produto[]) => {
+  //     this.listaProduto = resp
+  //   })
+  //   console.log(this.listaProduto)
+  // }
 }
--- Rento/src/app/edit/usuario-edit/usuario-edit.component.ts
+++ Rento/src/app/edit/usuario-edit/usuario-edit.component.ts
@@ -63,4 +63,5 @@ export class UsuarioEditComponent implements OnInit {
       this.usuario = resp
     })
   }
+ 
 }
